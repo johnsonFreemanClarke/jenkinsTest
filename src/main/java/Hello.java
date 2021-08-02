521c7fe0c370e5f67a684b6157b9e4ad2f993896
johnsonFreemanClarke <johnsonjwei@gmail.com>
2021-08-02 12:23:15 -0700

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        // Prints a message in the terminal window.
+        System.out.println("Hello, this is a simple Jenkins test.");
+    }
+}
